8e811e629cb811e140fa514a04b51259b6fb7f5b
timothywalker <timothywalker@live.com>
2015-11-09 22:36:23 +0000

    added validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+    has_many :pets, dependent: :destroy
+    validates :first_name, :last_name, :email, presence: true
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ActiveRecord::Base
+    belongs_to :person
+    validates :name, :variety, :person_id, presence: true
+    VARITEIES = %w(dog cat fish bird)
+    validates :variety, inclusion:{in: Pet::VARITEIES}
 end
